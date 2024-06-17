18d7aed72d10dbd612ceace040c922322364355c
abhiramibanil <abhiramib228@gmail.com>
2024-06-17 22:28:20 +0530

    Quiz-app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Quiz from '../src/Components/Quiz'
+import "./App.css"
+function App() {
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Dataforquiz/QuizData.jsx
@@ -0,0 +1,55 @@
+export const QuizData = [
+    {
+        question: "Numismatics is the study of ?",
+        options:[ " Coins", "Numbers", "Stamps", "Space"],
+        answer: 1
+    },
+    {
+        question: " Sound travels fastest through ?",
+        options:["Vaccum","Water", "Air", "Steel"],
+        answer: 4
+    },
+    {
+        question: " When ice melts in a beaker of water, the level of water in the?",
+        options: ["Increse","Decrese","Remains the Same","First increse and then decrese"],
+        answer: 3
+    },
+    {
+        question: "The language with the richest vocabulary?",
+        options: ["English", "Malayalam", "German", "Hindi"],
+        answer:1
+    },
+    {
+        question: "On which date noble prize is awarded?",
+        options: ["Dec 20", "Jan 10","Dec 10", "July 10"],
+        answer:3
+    },
+    {
+        question: "Harry Potter series is written by?",
+        options: ["KK Rowling","K J Rowling","AK Rowling","JK Rowling"],
+        answer:4
+    },
+    {
+        question: "How many medals are won in INDIAN history?",
+        options: ["9", "20", "90", "21"],
+        answer:2
+    },
+    {
+        question: "Fathometer is used to measure?",
+        options: ["Rain fall", "Ocean depth", "Sound intensity", "Earthquake"],
+        answer:2
+    },
+    {
+        question: "Epsom(England) is the place associated with?",
+        options: ["Snooing", "Polo", "Horse Racing", "Shooting"],
+        answer:3
+    },
+    
+    {
+        question: "Golf player Vijay Singh belongs to which country??",
+        options: ["Marcus", "Fiji", "Belgium", "London"],
+        answer:2
+    },
+
+   
+];
